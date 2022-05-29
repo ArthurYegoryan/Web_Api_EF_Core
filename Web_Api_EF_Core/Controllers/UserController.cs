09dacbfa78171b8a30b2bb0c36e7e25047d10be4
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-05-29 19:59:02 +0400

    Added migration with db and all commands

--- Web_Api_EF_Core/Controllers/UserController.cs
+++ Web_Api_EF_Core/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Api_EF_Core.Data;
 using Web_Api_EF_Core.Models;
 
 namespace Web_Api_EF_Core.Controllers
@@ -8,41 +10,75 @@ namespace Web_Api_EF_Core.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private static List<User> users = new List<User>
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
         {
-            new User {
-                Id = 1,
-                FirstName = "Aram",
-                LastName = "Aramyan",
-                Age = 25,
-                Email = "aa@gmail.com",
-                Phone = "099-99-99-99"},
-            new User {
-                Id = 2,
-                FirstName = "Sargis",
-                LastName = "Sargsyan",
-                Age = 30, Email = "ss@gmail.com",
-                Phone = "077-77-77-77"},
-            new User {
-                Id = 3,
-                FirstName = "Grigor",
-                LastName = "Grigoryan",
-                Age = 35,
-                Email = "gg@gmail.com",
-                Phone = "060-66-66-66"}
-        };
+            _context = context;
+        }
 
         [HttpGet]
-        public async Task<ActionResult<List<User>>> Get()
+        public async Task<ActionResult<List<User>>> GetUserList()
         {
-            return Ok(users);
+            //return Ok(users);
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            //User? user = users.Find(u => u.Id == id);
+            User? user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return BadRequest("User not found!");
+            }
+            return Ok(user);
         }
 
         [HttpPost]
         public async Task<ActionResult<List<User>>> AddUser(User user)
         {
-            users.Add(user);
-            return Ok(users);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User updateUser)
+        {
+            User? dbUser = await _context.Users.FindAsync(updateUser.Id);
+            if (dbUser == null)
+            {
+                return BadRequest("User not found!");
+            }
+
+            dbUser.FirstName = updateUser.FirstName;
+            dbUser.LastName = updateUser.LastName;
+            dbUser.Age = updateUser.Age;
+            dbUser.Email = updateUser.Email;
+            dbUser.Phone = updateUser.Phone;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            //User? user = users.Find(u => u.Id == id);
+            User? dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+            {
+                return BadRequest("User not found!");
+            }
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
         }
     }
 }
---
+++ Web_Api_EF_Core/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Api_EF_Core.Models;
+
+namespace Web_Api_EF_Core.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
